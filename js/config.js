4d0bb83ed0be7b5c7d9812c1ed89879ab6c9b88f
Gilles LE BRIS <gilles2.lebris@orange.com>
2019-06-20 08:24:56 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_0eJepqXPn', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'plad0k66fa32vlau0m78fvan7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
